81c502de6d5e215c04159d9fc6b6d592591d91d7
Lou Diamond T. Morados <83881490+loudiman@users.noreply.github.com>
2025-03-17 14:33:35 +0000

    Fixed removal of header model name

--- text_classification_models.py
+++ text_classification_models.py
@@ -46,7 +46,6 @@ selected_tfidf = st.sidebar.selectbox(
 )
 
 def load_model_components(model_name, selected_tfidf):
-    st.subheader(model_name)
     model = available_models[model_name]
     vectorizer = joblib.load(f"{TFIDF_DIR}/{selected_tfidf}.pkl")
     label_mapping = joblib.load(f"{MODEL_DIR}/{model_name}.pkl")
